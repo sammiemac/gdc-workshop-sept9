c553ffed506cf1b226821d75238ce3dc3b632403
pkflunr <71987718+pkflunr@users.noreply.github.com>
2022-09-08 22:12:39 -0700

    Modified Spikeball to be more simple

--- GodotWS/spikeball.gd
+++ GodotWS/spikeball.gd
@@ -1,9 +1,9 @@
-extends StaticBody2D
+extends Area2D
 
 
 func _ready():
 	pass
 
 
-func _on_Area2D_body_entered(body):
+func _on_spikeball_body_entered(body):
 	get_tree().change_scene("res://GameOver.tscn")
